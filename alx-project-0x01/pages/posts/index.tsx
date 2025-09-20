d4561b754227d94c28d199b9ad155a7c184aeb16
rihanas <rihanna.socials@gmail.com>
2025-09-20 03:40:37 +0300

    task 2

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Header from '@/components/layout/Header';
 import PostCard from '@/components/common/PostCard';
 
 const Posts: React.FC = () => {
   return (
-    <div className='p-8'>
-      <h1 className='text-3xl font-semibold mb-4'>Posts</h1>
-      <PostCard />
-      <PostCard />
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <main className='p-8 flex flex-col gap-4'>
+        <h1 className='text-3xl font-semibold mb-4'>Posts</h1>
+        <PostCard />
+        <PostCard />
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '@/components/layout/Header';
 
 const Users: React.FC = () => {
   return (
-    <div className='p-8'>
-      <h1 className='text-3xl font-semibold mb-4'>Users</h1>
-      <p>List of users will appear here...</p>
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <main className='p-8'>
+        <h1 className='text-3xl font-semibold mb-4'>Users</h1>
+        <p>List of users will appear here...</p>
+      </main>
     </div>
   );
 };
